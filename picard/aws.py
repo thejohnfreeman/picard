c0d6b1080747583b5de1c7943ea199c105aece13
John Freeman <jfreeman08@gmail.com>
2018-12-19 22:25:46 -0600

    Add comments

--- picard/aws.py
+++ picard/aws.py
@@ -7,23 +7,28 @@ necessary to create the resource. These parameters should be enough to
 identify the resource in a search.
 """
 
-from pathlib import Path
-
 import boto3 # type: ignore
 
 from picard.rule import rule
 
 def security_group(name, description=''):
     """An AWS security group."""
+
     @rule(name=name)
     async def security_group(context, self, inputs):
         # pylint: disable=unused-argument
+
+        # Search for the security group by name.
         client = boto3.client('ec2')
         response = client.describe_security_groups(GroupNames=[name])
         groups = response['SecurityGroups']
+
+        # More than one? Ambiguous.
         if len(groups) > 1:
             # TODO: Set the description.
             raise Exception(f'ambiguous security group name: {name}')
+
+        # Exactly one? Check for differences, then return it.
         ec2 = boto3.resource('ec2')
         if len(groups) == 1:
             group = groups[0]
@@ -37,11 +42,13 @@ def security_group(name, description=''):
                     f'actual: {actual}'
                 )
             return ec2.SecurityGroup(gid)
-        # The group does not exist. We must create it.
+
+        # None? We must create it.
         response = client.create_security_group(
             GroupName=name,
             Description=description)
         return ec2.SecurityGroup(response['GroupId'])
+
     return security_group
 
 
@@ -53,19 +60,28 @@ def key_pair(name):
     name :
         A name for the key pair.
     """
+
     @rule(name=name)
     async def key_pair(context, self, inputs):
         # pylint: disable=unused-argument
+
+        # Search for the key pair by name.
         client = boto3.client('ec2')
         response = client.describe_key_pairs(KeyNames=[name])
         key_pairs = response['KeyPairs']
+
+        # More than one? Ambiguous.
         if len(key_pairs) > 1:
             raise Exception(f'ambiguous key pair name: {name}')
+
+        # Exactly one? Return it.
         ec2 = boto3.resource('ec2')
         if len(key_pairs) == 1:
             return ec2.KeyPair(name)
-        # The key pair does not exist. We must create it.
+
+        # None? We must create it.
         response = client.create_key_pair(KeyName=name)
         return ec2.KeyPair(name)
         # Can we get the key material *after* the key has been created?
+
     return key_pair
--- tests/gitlab_runners.py
+++ tests/gitlab_runners.py
@@ -5,6 +5,9 @@ import picard.aws as aws
 
 sg = aws.security_group('gitlab-runners', 'GitLab Runners')
 kp = aws.key_pair('gitlab-runners-key')
+# We need to launch an instance, install software, and then save it as a new
+# Amazon Machine Image, instead of trying to install software on every new
+# instance. Do this out-of-band from the provisioning.
 
 if __name__ == '__main__':
     picard.main(kp)
