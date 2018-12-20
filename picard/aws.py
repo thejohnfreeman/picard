45b5917e85815f7644ea5a4dd9cb4b0ef482f14b
John Freeman <jfreeman08@gmail.com>
2018-12-19 20:45:13 -0600

    Add AWS key pairs

--- picard/aws.py
+++ picard/aws.py
@@ -7,6 +7,8 @@ necessary to create the resource. These parameters should be enough to
 identify the resource in a search.
 """
 
+from pathlib import Path
+
 import boto3 # type: ignore
 
 from picard.rule import rule
@@ -14,14 +16,14 @@ from picard.rule import rule
 def security_group(name, description=''):
     """An AWS security group."""
     @rule(name=name)
-    async def security_group_rule(context, state, inputs):
+    async def security_group(context, self, inputs):
         # pylint: disable=unused-argument
         client = boto3.client('ec2')
         response = client.describe_security_groups(GroupNames=[name])
         groups = response['SecurityGroups']
         if len(groups) > 1:
             # TODO: Set the description.
-            raise Exception(f'more than one security group with name: {name}')
+            raise Exception(f'ambiguous security group name: {name}')
         ec2 = boto3.resource('ec2')
         if len(groups) == 1:
             group = groups[0]
@@ -40,4 +42,30 @@ def security_group(name, description=''):
             GroupName=name,
             Description=description)
         return ec2.SecurityGroup(response['GroupId'])
-    return security_group_rule
+    return security_group
+
+
+def key_pair(name):
+    """An AWS key pair.
+
+    Parameters
+    ----------
+    name :
+        A name for the key pair.
+    """
+    @rule(name=name)
+    async def key_pair(context, self, inputs):
+        # pylint: disable=unused-argument
+        client = boto3.client('ec2')
+        response = client.describe_key_pairs(KeyNames=[name])
+        key_pairs = response['KeyPairs']
+        if len(key_pairs) > 1:
+            raise Exception(f'ambiguous key pair name: {name}')
+        ec2 = boto3.resource('ec2')
+        if len(key_pairs) == 1:
+            return ec2.KeyPair(name)
+        # The key pair does not exist. We must create it.
+        response = client.create_key_pair(KeyName=name)
+        return ec2.KeyPair(name)
+        # Can we get the key material *after* the key has been created?
+    return key_pair
--- tests/gitlab_runners.py
+++ tests/gitlab_runners.py
@@ -4,6 +4,7 @@ import picard
 import picard.aws as aws
 
 sg = aws.security_group('gitlab-runners', 'GitLab Runners')
+kp = aws.key_pair('gitlab-runners-key')
 
 if __name__ == '__main__':
-    picard.main(sg)
+    picard.main(kp)
